543e941f914c4f7ca04a3974cb72a9fddf031086
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2024-10-25 21:29:14 -0700

    upload proj3

---
+++ proj/proj3/queries/2a.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE VIEW labeled_data AS
+
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+
+(SELECT * FROM labeled_data WHERE is_outlier ORDER BY time, id LIMIT 50)
+UNION ALL
+(SELECT * FROM labeled_data WHERE NOT is_outlier ORDER BY time, id LIMIT 50);
---
+++ proj/proj3/queries/4ciii.sql
@@ -0,0 +1,6 @@
+CREATE OR REPLACE VIEW likely_data AS
+-- BEGIN SOLUTION
+
+-- END SOLUTION
+
+SELECT * FROM likely_data WHERE run_size > 2 ORDER BY id, time LIMIT 100;
